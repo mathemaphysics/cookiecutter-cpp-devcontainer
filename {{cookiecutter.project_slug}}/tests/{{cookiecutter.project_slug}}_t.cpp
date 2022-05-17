401ba1610983e51d2fe24e43afbc36ddab8ae06d
Ryan P. Daly <mathemaphysics@gmail.com>
2022-05-16 19:29:43 -0500

    Missed one area of Catch2 not converted to GTest

--- {{cookiecutter.project_slug}}/tests/{{cookiecutter.project_slug}}_t.cpp
+++ {{cookiecutter.project_slug}}/tests/{{cookiecutter.project_slug}}_t.cpp
@@ -1,10 +1,12 @@
 #include "{{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}.hpp"
-#include "catch2/catch.hpp"
+#include "gtest/gtest.h"
 
 using namespace {{ cookiecutter.project_slug.replace("-", "") }};
 
-TEST_CASE( "add_one", "[adder]" ){
-  REQUIRE(add_one(0) == 1);
-  REQUIRE(add_one(123) == 124);
-  REQUIRE(add_one(-1) == 0);
+TEST(AdditionTests, AddOneTest)
+{
+  EXPECT_EQ(add_one(0), 1);
+  EXPECT_EQ(add_one(123), 124);
+  EXPECT_EQ(add_one(-1), 0);
 }
+
