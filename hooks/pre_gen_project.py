e96f04b4f075385d306cd757cc5913f4e3432991
Ryan P. Daly <rdaly@herzog.com>
2023-03-31 14:13:43 -0500

    Removing unnecessary fields to give cookiecutter when building simple container.

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -12,34 +12,3 @@ def fail_if(condition, message):
     if condition:
         sys.stderr.write(message + "\n")
         sys.exit(1)
-
-
-fail_if(
-    "{{ cookiecutter.pypi_release }}" != "No" and "{{ cookiecutter.python_bindings }}" == "No",
-    "Can't do PyPI release without building Python bindings"
-)
-
-fail_if(
-    "{{ cookiecutter.pypi_release }}" != "No" and "{{ cookiecutter.github_actions_ci }}" == "No",
-    "Automatic PyPI releases are currently only supported in combination with Github Actions CI"
-)
-
-fail_if(
-    "{{ cookiecutter.codecovio }}" == "Yes" and "{{ cookiecutter.license }}" == "None",
-    "Coverage reports for codecov.io require an open source license for your project"
-)
-
-fail_if(
-    "{{ cookiecutter.sonarcloud }}" == "Yes" and "{{ cookiecutter.license }}" == "None",
-    "Code quality analysis from sonarcloud.io requires an open source license for your project"
-)
-
-fail_if(
-    "{{ cookiecutter.sonarcloud }}" == "Yes" and "{{ cookiecutter.github_actions_ci }}" == "No",
-    "Code quality analysis from sonarcloud.io is currently only supported in combination with Github Actions CI"
-)
-
-fail_if(
-    "{{ cookiecutter.sonarcloud }}" == "Yes" and "github.com" not in "{{ cookiecutter.remote_url }}",
-    "Code quality analysis from sonarcloud.io is currently only supported in combination with a Github remote repository"
-)
